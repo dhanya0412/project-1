535ab454c113e0dde3daedad4504cabf10f40f44
Raashi Sharma <sharmaraashi21@gamil.com>
2025-06-14 15:19:29 +0530

    made header section

---
+++ typedAnimation.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed(".auto-type", {
+    strings: ["FRONTEND-DEVELOPER", "UI/UX DESIGNER", "JAVASCRIPT ENTHUSIAST"],
+    typeSpeed: 200,
+    backSpeed: 300,
+    loop: true,
+
+});
