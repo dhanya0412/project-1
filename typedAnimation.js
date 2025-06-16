aaaf7bf1395ea80eda2a8ccda941d48eebb1b90e
dhanya0412 <dhanyagirdhar@gmail.com>
2025-06-16 16:58:01 +0530

    changes made

--- typedAnimation.js
+++ typedAnimation.js
@@ -2,7 +2,7 @@
 var typed = new Typed(".auto-type", {
     strings: ["FRONTEND-DEVELOPER", "UI/UX DESIGNER", "JAVASCRIPT ENTHUSIAST"],
     typeSpeed: 100,
-    backSpeed: 200,
+    backSpeed: 100,
     loop: true,
 
 });
